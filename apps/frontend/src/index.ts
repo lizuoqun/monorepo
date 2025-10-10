9906739430d89bf2285958d79bc0ec99addf67b0
modify <2645929971@qq.com>
2025-10-10 10:28:16 +0800

    chore(eslint): 配置ESLint并集成Prettier和Vue插件 - 添加ESLint配置文件，支持TypeScript和Vue - 集成Prettier插件实现代码格式化 - 配置前端和后端不同的lint规则 - 添加ESLint脚本到package.json - 更新依赖项，包括eslint、typescript-eslint等 - 忽略特定目录和文件类型不进行lint检查 - 在前端代码中添加测试用的var声明以验证规则生效

--- apps/frontend/src/index.ts
+++ apps/frontend/src/index.ts
@@ -1,1 +1,3 @@
 console.log('hello frontend');
+var a = 10;
+console.log(a);
---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import {defineConfig} from 'eslint/config';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+
+// 忽略的文件
+const ignores = [
+  '**/dist/**',
+  '**/node_modules/**',
+  '.*',
+  'scripts/**',
+  '**/*.d.ts'
+];
+
+export default defineConfig(
+  // 通用配置
+  {
+    ignores,
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+    plugins: {prettier: eslintPluginPrettier},
+    languageOptions: {
+      ecmaVersion: 'latest', // ECMAScript 版本
+      sourceType: 'module', // 模块化类型
+      parser: tseslint.parser // 解析器
+    },
+    rules: {
+      // 自定义规则
+      // 禁止使用 var
+      'no-var': 'error'
+    }
+  },
+  // 前端配置
+  {
+    ignores,
+    files: [
+      'app/frontend/**/*.{js,jsx,ts,tsx,vue}',
+      'packages/components/**/*.{js,jsx,ts,tsx,vue}'
+    ],
+    extends: [
+      ...eslintPluginVue.configs['flat/recommended'],
+      eslintConfigPrettier
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+  // 后端配置
+  {
+    ignores,
+    files: ['apps/backend/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  }
+);

b39c0896a1bc7b14d0618539efa42f5c03f4ce19
modify <2645929971@qq.com>
2025-10-14 09:59:06 +0800

    build: 配置 Rollup 打包优化 - 添加 `@rollup/plugin-terser` 插件以压缩输出文件 - 引入 `rollup-plugin-clear` 自动清理 dist 目录 - 修改构建命令支持监听模式 (`-w` 参数) - 更新 Rollup 配置为多输出格式（压缩版与非压缩版） - 启用构建前自动清除旧文件功能 - 调整入口配置结构以适应多文件打包需求

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,33 @@
-import { babel } from '@rollup/plugin-babel';
+import {babel} from '@rollup/plugin-babel';
+import clear from 'rollup-plugin-clear';
+import terser from '@rollup/plugin-terser';
 
 export default {
-  input: './scripts/main.ts',
-  output: {
-    // 单入口打包只需要指定file，不需要指定dir（多入口打包需要指定dir）
-    // dir: 'dist',
-    file: './dist/bundle.js',
-    format: 'iife',
-    name: 'modify'
+  // 单入口打包
+  // input: './scripts/main.ts',
+  input: {
+    index: './scripts/main.ts'
   },
-  plugins: [babel({ babelHelpers: 'runtime', exclude: 'node_modules/**', extensions: ['.js', '.jsx', 'ts', '.tsx'] })]
+  output: [{
+    // 单入口用这个指定打包文件名
+    // file: './dist/bundle.js',
+    // 多入口用这个指定打包目录
+    dir: 'dist',
+    format: 'iife',
+    entryFileNames: '[name].min.js',
+    plugins: [terser()]
+  }, {
+    dir: 'dist',
+    format: 'iife',
+    entryFileNames: '[name].js'
+  }],
+  plugins: [
+    // babel 代码降级插件
+    babel({babelHelpers: 'runtime', exclude: 'node_modules/**', extensions: ['.js', '.jsx', 'ts', '.tsx']}),
+    // 清除 dist 目录插件
+    clear({
+      targets: ['dist'],
+      watch: true
+    })
+  ]
 };
